68ac49c06c5127d35247a6ae2e06719afb4ef093
Anna Nikolaeva <amnik21@yandex.ru>
2018-08-02 16:16:40 +0300

    Импорт модуля (функция расчета площади треугольника)

---
+++ myMathModule.js
@@ -0,0 +1,6 @@
+export function getTriangleArea(h, b) {
+    var a = 1/2 * h * b; 
+    return a;
+  }
+var area = getTriangleArea(3, 2);
+console.log(area);
---
+++ solution.js
@@ -0,0 +1,6 @@
+import {getTriangleArea} from "./myMathModule.js";
+function getArea(n) {
+  var a = 1 / 2 * n * square (n) / 2;
+  return a;
+  };
+  export default getArea;
